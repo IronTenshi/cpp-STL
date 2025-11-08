befb893feb5b9c0b245a0990e51c034800c5bc06
IronTenshi <2356128385@qq.com>
2025-11-08 20:43:23 +0800

    add something

--- binary tree expands.cpp
+++ binary tree expands.cpp
@@ -1,4 +1,35 @@
 #include<iostream>
 
-
+template<typename datatype>
 struct root
+{
+    datatype data;
+    root<datatype>* left;
+    root<datatype>* right;
+    root(datatype value) : data(value), left(nullptr), right(nullptr) {}//constructor,initializing nullptrs;
+};
+
+template<typename datatype>
+class binary_tree
+{
+private:
+    root<datatype>* tree_root;
+    void PreDisplay();
+
+public:
+    binary_tree()//initailizing the root 
+    {
+        tree_root = new root<datatype>(datatype());//create a stack object of the datatype;
+        tree_root = nullptr;
+    }
+
+    void PreCreate();
+    void InorderCreate();
+    int GetHeight(root<datatype>* node);
+    int GetLeavesCount();
+    int GetCount();
+    int PreDisplayLevels();
+    int OnlyChildCount();
+    int OnlyLeftChildCount();
+    int FullChildCount();
+};
